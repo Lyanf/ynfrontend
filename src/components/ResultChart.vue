6447b311be6d93ddec7cef2ea8d2dad569c3c81f
yuetsin <akaza_akari@outlook.com>
2021-01-06 11:14:35 +0800

    [add] chart saving

--- src/components/ResultChart.vue
+++ src/components/ResultChart.vue
@@ -5,7 +5,7 @@
     </el-row>
     <el-row>
       <el-col :span="2">
-        <el-button type="primary">图像导出</el-button>
+        <el-button type="primary" v-on:click="exportImage">图像导出</el-button>
       </el-col>
     </el-row>
   </div>
@@ -14,16 +14,33 @@
 <script>
 import * as echarts from 'echarts';
 
+function base64ToBlob(code) {
+  const parts = code.split(';base64,');
+  const contentType = parts[0].split(':')[1];
+  const raw = window.atob(parts[1]);
+  const rawLength = raw.length;
+
+  const uInt8Array = new Uint8Array(rawLength);
+
+  // eslint-disable-next-line no-plusplus
+  for (let i = 0; i < rawLength; ++i) {
+    uInt8Array[i] = raw.charCodeAt(i);
+  }
+  return new Blob([uInt8Array], { type: contentType });
+}
+
 export default {
   name: 'ResultChart',
   data() {
     return {
       temp: '',
+      currentChart: undefined,
     };
   },
   methods: {
     initializeChart() {
       const chart1 = echarts.init(document.getElementById('chart1'));
+      this.currentChart = chart1;
       const initializeOption = {
         xAxis: {
           type: 'category',
@@ -38,6 +55,21 @@ export default {
         }],
       };
       chart1.setOption(initializeOption);
+    },
+    exportImage() {
+      if (!this.currentChart) {
+        return;
+      }
+      const content = this.currentChart.getDataURL();
+
+      const aLink = document.createElement('a');
+      const blob = base64ToBlob(content);
+
+      const evt = document.createEvent('HTMLEvents');
+      evt.initEvent('click', true, true);
+      aLink.download = 'charts.png';
+      aLink.href = URL.createObjectURL(blob);
+      aLink.dispatchEvent(new MouseEvent('click', { bubbles: true, cancelable: true, view: window }));
     },
   },
   mounted() {
