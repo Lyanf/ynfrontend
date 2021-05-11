478d060d0c49eb99c7a8878b4441d661af403e0c
yuetsin <akaza_akari@outlook.com>
2021-05-11 10:22:29 +0800

    我有病

--- src/components/SelectForm/MutableForm.vue
+++ src/components/SelectForm/MutableForm.vue
@@ -165,7 +165,6 @@ export default {
       const data = json2csv.parse(raw, {
         fields: ['year'].concat(params.rejectlsit),
       });
-      alert(data);
       const blob = new Blob([iconv.encode(data, 'gbk')], { type: 'text/csv;charset=utf-8' });
       saveAs(blob, '剔除数据模版.csv');
     },
