6d5ebeadf0eacfa5347e227c10b974f57d25fe93
yuetsin <akaza_akari@outlook.com>
2021-05-11 13:40:50 +0800

    v0.1.31

--- src/components/ResultTable.vue
+++ src/components/ResultTable.vue
@@ -1,6 +1,7 @@
 <template>
   <el-form>
-    <el-form-item label="评价指标表：" v-if="nomapermse === undefined">
+    <el-form-item label="评价指标表："
+                  v-if="nomapermse === undefined && tableOneData.length !== 0">
       <el-table :data="tableOneData">
 <!--        <el-table-column prop="index" label="评价指标"></el-table-column>-->
 <!--        <el-table-column prop="r2" label="R2"></el-table-column>-->
@@ -8,19 +9,17 @@
         <el-table-column prop="rmse" label="RMSE"></el-table-column>
       </el-table>
     </el-form-item>
-    <el-form-item v-if="nomapermse === undefined">
-      <el-button @click="exportTableOneSheet"
-                 :disabled="tableOneData.length === 0">导出评价指标表</el-button>
+    <el-form-item v-if="nomapermse === undefined && tableOneData.length !== 0">
+      <el-button @click="exportTableOneSheet">导出评价指标表</el-button>
     </el-form-item>
-    <el-form-item label="年份 − 预测值表：">
+    <el-form-item label="年份 − 预测值表：" v-if="tableTwoData.length !== 0">
       <el-table :data="tableTwoData">
         <el-table-column prop="year" label="年份"></el-table-column>
         <el-table-column prop="predict" label="预测值（MW）"></el-table-column>
       </el-table>
     </el-form-item>
-    <el-form-item>
-      <el-button @click="exportTableTwoSheet"
-                 :disabled="tableTwoData.length === 0">导出预测结果表</el-button>
+    <el-form-item v-if="tableTwoData.length !== 0">
+      <el-button @click="exportTableTwoSheet">导出预测结果表</el-button>
     </el-form-item>
   </el-form>
 </template>
@@ -47,7 +46,7 @@ export default {
       saveAs(blob, 'database.csv');
     },
     exportTableOneSheet() {
-      this.exportTableSheet(this.$data.tableOneData, ['index', 'r2', 'mape', 'rmse']);
+      this.exportTableSheet(this.$data.tableOneData, ['mape', 'rmse']);
     },
     exportTableTwoSheet() {
       this.exportTableSheet(this.$data.tableTwoData, ['year', 'predict']);
--- src/views/PredictResultQuery.vue
+++ src/views/PredictResultQuery.vue
@@ -2,7 +2,7 @@
   <div>
     <el-row>
       <el-col :span="22" :offset="1">
-        <el-form>
+        <el-form inline>
           <el-form-item label="方案标签：">
             <el-select v-model="selectedTag">
               <el-option v-for="item in knownTags"
@@ -17,31 +17,93 @@
             <el-button type="primary" :disabled="selectedTag === null"
                        @click="preformQuery">查询</el-button>
           </el-form-item>
+        </el-form>
+<!--        <el-form inline v-if="predictTokens !== undefined">-->
+<!--          <el-form-item label="MAPE：">{{ predictTokens.mape }}</el-form-item>-->
+<!--          <el-form-item label="RMSE：">{{ predictTokens.rmse }}</el-form-item>-->
+<!--        </el-form>-->
+        <el-row>
+          <div id="predictChart" style="height: 500px"></div>
+        </el-row>
+        <el-row v-if="currentChart !== undefined">
+          <el-form>
+            <el-form-item>
+              <el-button v-on:click="exportImage">导出图像</el-button>
+            </el-form-item>
+            <el-form-item label="显示方式：">
+              <el-select placeholder="请选择" v-model="currentDisplayMethod">
+                <el-option
+                  v-for="item in displayMethods"
+                  :key="item.value"
+                  :label="item.label"
+                  :value="item.value">
+                </el-option>
+              </el-select>
+            </el-form-item>
+          </el-form>
+        </el-row>
+        <ResultTable ref="resultTable"></ResultTable>
+        <el-form>
           <el-form-item label="方案参数：" v-if="jsonContent !== undefined" />
           <json-viewer v-if="jsonContent !== undefined" :value="jsonContent" copyable></json-viewer>
-
           <el-form-item label="预测结果：" v-if="predictContent !== undefined" />
           <json-viewer v-if="predictContent !== undefined"
                        :value="predictContent" copyable></json-viewer>
         </el-form>
       </el-col>
-
     </el-row>
   </div>
 </template>
 
 <script>
 import JsonViewer from 'vue-json-viewer';
+import * as echarts from 'echarts';
+import { saveAs } from 'file-saver';
+import ResultTable from '@/components/ResultTable.vue';
+
+function base64ToBlob(code) {
+  const parts = code.split(';base64,');
+  const contentType = parts[0].split(':')[1];
+  const raw = window.atob(parts[1]);
+  const rawLength = raw.length;
+
+  const uInt8Array = new Uint8Array(rawLength);
+
+  // eslint-disable-next-line no-plusplus
+  for (let i = 0; i < rawLength; ++i) {
+    uInt8Array[i] = raw.charCodeAt(i);
+  }
+  return new Blob([uInt8Array], { type: contentType });
+}
 
 export default {
   name: 'PredictResultQuery',
-  components: { JsonViewer },
+  components: { JsonViewer, ResultTable },
   data() {
     return {
       knownTags: [],
       selectedTag: null,
       jsonContent: undefined,
       predictContent: undefined,
+      predictTokens: undefined,
+      predictPayloads: [],
+      currentChart: undefined,
+      chartData: [],
+      displayMethods: [
+        {
+          label: '折线图',
+          value: 'line',
+        },
+        {
+          label: '柱状图',
+          value: 'bar',
+        },
+        {
+          label: '散点图',
+          value: 'scatter',
+        },
+      ],
+      currentDisplayMethod: 'line',
       typeName: {
         MINING: '数据挖掘方案',
         STATIC_REGIONAL: '地区单预测方案',
@@ -60,6 +122,14 @@ export default {
     this.loadTags();
   },
   methods: {
+    exportImage() {
+      if (!this.currentChart) {
+        return;
+      }
+      const content = this.currentChart.getDataURL();
+      const blob = base64ToBlob(content);
+      saveAs(blob, '预测图像.png');
+    },
     loadTags() {
       this.$axios.get('/predict/results/query').then((response) => {
         this.$data.knownTags = response.data.data;
@@ -71,9 +141,120 @@ export default {
           tag: this.$data.selectedTag,
         },
       }).then((response) => {
-        this.$data.jsonContent = response.data.data.arg;
-        this.$data.predictContent = response.data.data.result;
+        const content = response.data.data;
+        this.$data.jsonContent = content.arg;
+        this.$data.predictContent = content.result;
+        this.$data.predictTokens = {
+          mape: content.result.MAPE,
+          rmse: content.result.RMSE,
+        };
+        const years = [];
+        for (let i = content.arg.PreStartYear; i <= content.arg.PreEndYear; i += 1) {
+          years.push(i);
+        }
+        this.$data.chartData = {
+          xName: '年份',
+          xData: years,
+          yName: '预测值（MW）',
+          yData: [
+            {
+              data: content.result.preresult,
+              tag: content.arg.tag,
+            }],
+        };
+        this.refreshChart();
+        const takeTwoData = [];
+        for (let i = content.arg.PreStartYear; i <= content.arg.PreEndYear; i += 1) {
+          takeTwoData.push({
+            year: i,
+            predict: content.result.preresult[i - content.arg.PreStartYear],
+          });
+        }
+        this.$refs.resultTable.tableOneData = [this.$data.predictTokens];
+        this.$refs.resultTable.tableTwoData = takeTwoData;
       });
+    },
+    refreshChart() {
+      const data = this.$data.chartData;
+      if (this.currentChart === undefined) {
+        this.currentChart = echarts.init(document.getElementById('predictChart'));
+        window.onresize = this.currentChart.resize;
+      }
+      const series = [];
+      data.yData.forEach((elem) => {
+        series.push({
+          data: elem.data,
+          name: elem.tag,
+          type: this.$data.currentDisplayMethod,
+        });
+      });
+      const initializeOption = {
+        // color: ['#DBDA39'],
+        tooltip: {
+          trigger: 'axis',
+          axisPointer: {
+            type: 'shadow',
+          },
+        },
+        xAxis: {
+          type: 'category',
+          name: data.xName,
+          data: data.xData,
+          axisTick: {
+            alignWithLabel: true,
+          },
+          axisLabel: {
+            show: true,
+            textStyle: {
+              color: '#ffffff',
+              fontSize: 14,
+            },
+          },
+          axisLine: {
+            lineStyle: {
+              color: '#ffffff',
+            },
+          },
+          boundaryGap: this.$data.currentDisplayMethod === 'bar',
+          onZero: true,
+        },
+        yAxis: {
+          type: 'value',
+          name: data.yName,
+          axisLabel: {
+            show: true,
+            textStyle: {
+              color: '#ffffff',
+              fontSize: 14,
+            },
+          },
+          axisLine: {
+            lineStyle: {
+              color: '#ffffff',
+            },
+          },
+        },
+        series,
+        legend: {
+          // orient 设置布局方式，默认水平布局，可选值：'horizontal'（水平） ¦ 'vertical'（垂直）
+          orient: 'horizontal',
+          // x 设置水平安放位置，默认全图居中，可选值：'center' ¦ 'left' ¦ 'right' ¦ {number}（x坐标，单位px）
+          x: 'center',
+          // y 设置垂直安放位置，默认全图顶端，可选值：'top' ¦ 'bottom' ¦ 'center' ¦ {number}（y坐标，单位px）
+          y: 'top',
+          textStyle: {
+            color: '#ffffff',
+            fontSize: 16,
+          },
+        },
+      };
+      console.log(initializeOption);
+      this.currentChart.setOption(initializeOption, true);
+    },
+  },
+  watch: {
+    currentDisplayMethod() {
+      this.refreshChart();
     },
   },
 };
