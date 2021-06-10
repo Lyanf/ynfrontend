32f84a8de6865da943a81c0f43ad5001a7b2b4ae
yuetsin <akaza_akari@outlook.com>
2021-06-10 08:38:50 +0800

    [fix] unit display

--- src/views/FuHeTeXingCompute.vue
+++ src/views/FuHeTeXingCompute.vue
@@ -496,7 +496,7 @@ export default {
         },
         yAxis: {
           type: 'value',
-          name: data.yName,
+          name: data.xName.endsWith('（MW）') ? '单位：MW' : '',
         },
         series,
         legend: {
